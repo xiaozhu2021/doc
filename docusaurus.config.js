8f4c0039105f3d788db7981b3a9a5b056338d79e
甜力怕 <63779340+xiaozhu2007@users.noreply.github.com>
2021-02-21 12:06:57 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -86,7 +86,7 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/xiaozhu2021/doc/edit/master/website/',
+            'https://github.com/xiaozhu2021/doc/edit/master/doc/',
         },
         blog: {
           showReadingTime: true,
